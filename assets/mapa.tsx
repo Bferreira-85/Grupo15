3f4fe94e438bbe5329a43be0a7b2c536ad06c4ca
Dylane Esteves <estevesdylane98@gmail.com>
2025-04-18 17:12:22 +0100

    change of  world map
    change of map

---
+++ assets/mapa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="mapa" tilewidth="16" tileheight="16" tilecount="5104" columns="58">
+ <image source="world.png" width="936" height="1408"/>
+</tileset>
---
+++ core/src/main/java/com/bd2r/harrypotter/Screen/Play.java
@@ -0,0 +1,56 @@
+package com.bd2r.harrypotter.Screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class Play implements Screen {
+
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+    @Override
+    public void show() {
+        map = new TmxMapLoader().load("mundo.tmx");
+
+        renderer = new OrthogonalTiledMapRenderer(map);
+    }
+
+    @Override
+    public void render(float v) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        map.dispose();
+        renderer.dispose();
+    }
+}
